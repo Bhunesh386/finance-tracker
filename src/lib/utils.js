72ba21794a233c09eded8d801ec7a04b9b400ad7
bhunesh Sharma <bhuvanesh8887@gmail.com>
2026-03-14 16:32:09 +0530

    Initial code for the site

--- src/lib/utils.js
+++
@@ -1,7 +0,0 @@
-export const formatCurrency = (amount) => {
-  return Number(amount).toLocaleString('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-    maximumFractionDigits: 0,
-  });
-};
